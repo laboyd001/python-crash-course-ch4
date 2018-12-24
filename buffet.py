3dce1c91c4df1df6559eafde2ff8e37a4081244f
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-24 11:07:33 -0600

    exercises for tuples

---
+++ buffet.py
@@ -0,0 +1,16 @@
+#use a for loop to print each food, try to modify one item, add code to rewrite the tuple
+
+buffet = ('salad', 'ham', 'chicken', 'pizza', 'rolls')
+for food in buffet:
+  print(food)
+
+
+#Cannot modify one item:
+# buffet[0] = ('jello')
+# for food in buffet:
+#   print(food)
+
+print("\nUpdate buffet menu:")
+buffet = ('salad', 'ham', 'egg salad', 'soup', 'rolls')
+for food in buffet:
+  print(food)
