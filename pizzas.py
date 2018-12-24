e13833677c3be12236dd7c3865635c491f8ca4a1
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-24 09:28:55 -0600

    string for loops

---
+++ pizzas.py
@@ -0,0 +1,8 @@
+#this is a program that demos a for loop
+
+
+pizzas = ['cheese', 'peperoni', 'supreme']
+for pizza in pizzas:
+  # print(pizza)
+  print("I like " + pizza + " pizza!")
+print("\nI really love pizza!")
